729e133ba9df50c7587f6c136e0a3eaed959bb8d
Giulia Falaschi <giuliafalaschi@Mac.wind3.hub>
2025-08-16 16:36:39 +0200

    feat: aggiunta delle operazioni mancanti

--- app/app.py
+++ app/app.py
@@ -24,11 +24,53 @@ def sub():
 
 #Endpoint /mul for multiplication which takes a and b as query parameters.
 
+@app.route('/mul')
+def mul():
+    a = request.args.get('a', type=float)
+    b = request.args.get('b', type=float)
+    if a and b:
+        return make_response(jsonify(s=a*b), 200) #HTTP 200 OK
+    else:
+        return make_response('Invalid input\n', 400) #HTTP 400 BAD REQUEST
+
 #Endpoint /div for division which takes a and b as query parameters. Returns HTTP 400 BAD REQUEST also for division by zero.
 
+@app.route('/div')
+def div():
+    a = request.args.get('a', type=float)
+    b = request.args.get('b', type=float)
+    if a and b:
+        if b == 0:
+            return make_response('Division by zero is not allowed\n', 400) #HTTP 400 BAD REQUEST
+        return make_response(jsonify(s=a/b), 200) #HTTP 200 OK
+    else:
+        return make_response('Invalid input\n', 400) #HTTP 400 BAD REQUEST
+
 #Endpoint /mod for modulo which takes a and b as query parameters. Returns HTTP 400 BAD REQUEST also for division by zero.
+@app.route('/mod')
+def mod():
+    a = request.args.get('a', type=float)
+    b = request.args.get('b', type=float)
+    if a and b:
+        if b == 0:
+            return make_response('Division by zero is not allowed\n', 400) #HTTP 400 BAD REQUEST
+        return make_response(jsonify(s=a%b), 200) #HTTP 200 OK
+    else:
+        return make_response('Invalid input\n', 400) #HTTP 400 BAD REQUEST  
 
 #Endpoint /random which takes a and b as query parameters and returns a random number between a and b included. Returns HTTP 400 BAD REQUEST if a is greater than b.
+
+@app.route('/random')
+def random_number():
+    import random
+    a = request.args.get('a', type=float)
+    b = request.args.get('b', type=float)
+    if a is not None and b is not None:
+        if a > b:
+            return make_response('Invalid range: a should be less than or equal to b\n', 400) #HTTP 400 BAD REQUEST
+        return make_response(jsonify(s=random.uniform(a, b)), 200) #HTTP 200 OK
+    else:
+        return make_response('Invalid input\n', 400) #HTTP 400 BAD REQUEST
 
 if __name__ == '__main__':
     app.run(debug=True)
